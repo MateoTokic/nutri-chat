a1805e54617e928d18bf4319b485d3e7dc649d4a
Mateo Tokic <mateo.tokic1@gmail.com>
2024-11-24 18:42:31 +0100

    commit firebase

--- nutri-app-mobile/services/config.js
+++ nutri-app-mobile/services/config.js
@@ -6,12 +6,12 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "",
-  authDomain: "nutri-app-mobile.firebaseapp.com",
-  projectId: "nutri-app-mobile",
-  storageBucket: "nutri-app-mobile.appspot.com",
-  messagingSenderId: "157008882352",
-  appId: "1:157008882352:web:e5014002d538e8fbde6975",
-  measurementId: "G-E656FGJCV1",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
